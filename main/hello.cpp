7c5050a5935613e019ba8b970533e8be0388d375
chaitra2308 <pchaitra08@yahoo.com>
2024-03-06 18:15:53 +0530

    After adding webhook trigger

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "PES1UG21CS145\n";
   cout << "Hello, Jenkins\n";
-  cout << "Executing after adding webhook!\n";
+  cout << "Executing after adding webhook trigger!\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
